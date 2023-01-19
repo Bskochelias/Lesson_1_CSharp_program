9b301108d297b90b989b3a67a377e8811e340501
Boleslav <bolik.kg@gmail.com>
2023-01-19 22:31:09 +0600

    Add Random for Example003_Sum

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,9 @@
-﻿int NumberA = 3;
-int NumberB = 5;
+﻿int NumberA = new Random().Next(1,100);
+int NumberB = new Random().Next(1,20);
+Console.Write("Первое число:");
+Console.WriteLine(NumberA);
+Console.Write("Второе число:");
+Console.WriteLine(NumberB);
 int result = NumberA + NumberB;
+Console.Write("Сумма равна:");
 Console.WriteLine(result);
